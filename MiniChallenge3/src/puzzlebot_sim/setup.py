9542ed1f4ef444081fb697a7a24a8c0524f3d3f6
Fiona Stasi Fernández <stasifiona@gmail.com>
2026-04-21 17:51:04 -0600

    Add missing data files for launch, config, rviz, urdf, and meshes in setup.py

--- MiniChallenge3/src/puzzlebot_sim/setup.py
+++ MiniChallenge3/src/puzzlebot_sim/setup.py
@@ -1,3 +1,6 @@
+import os
+from glob import glob
+
 from setuptools import find_packages, setup
 
 package_name = 'puzzlebot_sim'
@@ -10,6 +13,11 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        (os.path.join('share', package_name, 'config'), glob('config/*')),
+        (os.path.join('share', package_name, 'rviz'), glob('rviz/*')),
+        (os.path.join('share', package_name, 'urdf'), glob('urdf/*')),
+        (os.path.join('share', package_name, 'meshes'), glob('meshes/*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
